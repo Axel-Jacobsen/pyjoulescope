bbaaa5e23e523d23cf7cc380e4f044edb716afcb
Axel Jacobsen <axel@wildlifecomputers.com>
2019-07-24 16:04:46 -0700

    add basic window method and speed optimizations
    optimize cdf

--- post_process.py
+++ post_process.py
@@ -1,17 +1,19 @@
 import time
 import math
 import numpy as np
-from joulescope.data_recorder import DataReader
 from typing import Callable
+from collections import deque
+from joulescope.data_recorder import DataReader
+
+CHUNK_SIZE = 2**16
 
 
 class PostProcess:
-
     def __init__(self):
         pass
 
     def histogram(self,
-                  fname: str,
+                  reader: DataReader,
                   t1: float = None,
                   t2: float = None,
                   signal: str = 'current') -> (np.array, float, float):
@@ -25,71 +27,94 @@ class PostProcess:
             width: number to multiply bin index by to get the x value of the bin
             min: minimum data value
         """
-        CHUNK_SIZE = 2**16
         signal_index = _get_signal_index(signal)
-
-        if not fname.endswith('jls'):
-            raise RuntimeError('file must be .jls type')
-
-        reader = DataReader()
-        reader.open(fname)
 
         _t1 = t1 if t1 else 0
         _t2 = t2 if t2 else reader.duration
 
-        # start and end data id - i.e. range of data that we are calculating
         id_start = reader.time_to_sample_id(_t1)
         id_end = reader.time_to_sample_id(_t2)
 
-        # Get statistics on the signal (i.e. voltage, current) that we want
-        # `statistics` is a dictionary of μ, σ, max, min, p2p
         statistics = reader.statistics_get(_t1, _t2)['signals'][signal]['statistics']
         maximum, minimum = statistics['max'], statistics['min']  # when does max/min from statistics not equal max/min from _get_max_min?
-        # print(minimum, maximum)
-        # print(statistics['min'], statistics['max'])
 
-        # num_bins = (1 + math.ceil(math.log(id_end, 2)))
         h = 3.5 * statistics['σ'] / (id_end - id_start)**(1. / 3)
         num_bins = math.ceil((maximum - minimum) / h)
+
+        _binning = lambda v: math.floor((v - minimum) * (num_bins - 1) / (maximum - minimum))
         hist = np.zeros(num_bins)
 
-        # _binning: real in [minimum, maximum] -> integer in [0, num_bins)
-        _binning = lambda v: math.floor((v - minimum) * (num_bins - 1) / (maximum - minimum))
-
         for i in range(id_start, id_end, CHUNK_SIZE):
-            print('progress: {:.3} %\t\r'.format((i / id_end) * 100), end='')
+            print('progress: {:.3} %\t\r'.format(((i - id_start) / (id_end - id_start)) * 100), end='')
             start = i
             end = i + CHUNK_SIZE if i + CHUNK_SIZE < id_end else id_end
 
             data = reader.get_calibrated(start, end)
 
-            for i in range(len(data[signal_index])):
-                hist[_binning(data[signal_index][i])] += 1
+            for v in data[signal_index]:
+                hist[_binning(v)] += 1
 
         reader.close()
         hist /= (id_end - id_start)  # Normalize histogram bin counts
-        width = h  #(maximum - minimum) / num_bins
+        width = h
 
         return hist, width, minimum
 
-    def cdf(self, fname: str, signal: str = 'current'):
+    def max_window(self, reader, duration: int):
+        signal_index = _get_signal_index('current')
+        id_start, id_end = reader.sample_id_range
+        id_duration = reader.time_to_sample_id(duration)
+        queue = deque(maxlen=id_duration)
+
+        start = 0
+        end = id_duration
+        data = reader.get_calibrated(start, end)
+        for i in range(id_start, id_duration):
+            queue.append(data[signal_index][i])
+
+        max = sum(queue)
+        current_sum = max
+        start_mark = 0
+        end_mark = id_duration
+
+        # There is probably some numpy way to do this that could also order you coffee at the same time,
+        # but I am not sure what that could be and this is already fast enough for the point
+        # of development that I am at currently
+        for i in range(id_duration, id_end, CHUNK_SIZE):
+            print('progress: {:.3} %\t\r'.format(((i) / id_end) * 100), end='')
+            start = i
+            end = i + CHUNK_SIZE if i + CHUNK_SIZE < id_end else id_end
+
+            data = reader.get_calibrated(start, end)
+            for j, new_value in enumerate(data[signal_index]):
+                discard_val = queue.popleft()
+                queue.append(new_value)
+                current_sum += new_value - discard_val
+                if current_sum > max:
+                    max = current_sum
+                    start_mark = i + j - id_duration
+                    end_mark = i + j
+
+        return max, start_mark, end_mark
+
+    def cdf(self, reader: DataReader, signal: str = 'current'):
         """
         Cumulative Distribution function
         """
-        hist, width, minimum = self.histogram(fname, signal)
+        hist, width, minimum = self.histogram(reader, signal=signal)
 
         _cdf = np.zeros(len(hist))
         _cdf[0] = hist[0]
-        for i in range(1, len(hist)):
-            _cdf[i] = _cdf[i - 1] + hist[i]
+        for i, hist_val in enumerate(hist, 1):
+            _cdf[i] = _cdf[i - 1] + hist_val
 
         return _cdf, width, minimum
 
-    def ccdf(self, fname: str, signal: str = 'current'):
+    def ccdf(self, reader: DataReader, signal: str = 'current'):
         """
         Complementary Cumulative Distribution Function
         """
-        _cdf, width, minimum = self.cdf(fname, signal)
+        _cdf, width, minimum = self.cdf(reader, signal=signal)
         return 1 - _cdf, width, minimum
 
 
@@ -104,7 +129,6 @@ def _get_signal_index(signal: str):
 
 
 def _get_max_min(reader_obj, id_start, id_end):
-    CHUNK_SIZE = 2**16
     maximum = 0
     minimum = 1e9  # assuming we are not consuming gigaamps or gigavolts through joulescope
 
@@ -116,22 +140,57 @@ def _get_max_min(reader_obj, id_start, id_end):
         data = reader_obj.get_calibrated(start, end)
         for i in range(len(data[0])):
             v = data[0][i]
-            if v > maximum:
-                maximum = v
-            elif v < minimum:
-                minimum = v
+            maximum = max(v, maximum)
+            minimum = min(v, minimum)
 
     return maximum, minimum
 
 
-if __name__ == '__main__':
+def hist_main():
     import matplotlib.pyplot as plt
+    fname = 'sample_data/shorty.jls'
+
+    reader = DataReader()
+    reader.open(fname)
 
     p = PostProcess()
-    vs, width, minimum = p.ccdf('sample_data/cleaner.jls')
+    vs, width, minimum = p.histogram(reader)
     xs = [i * width + minimum for i in range(len(vs))]
 
     plt.bar(xs, vs, width=width)
     plt.xlabel('Current (mA)')
     plt.ylabel('Probability')
     plt.show()
+
+def cdf_main():
+    import matplotlib.pyplot as plt
+    fname = 'sample_data/cleaner.jls'
+
+    reader = DataReader()
+    reader.open(fname)
+
+    p = PostProcess()
+    vs, width, minimum = p.cdf(reader)
+    xs = [i * width + minimum for i in range(len(vs))]
+
+    plt.plot(xs, vs)
+    plt.xlabel('Current (mA)')
+    plt.ylabel('Probability')
+    plt.show()
+
+
+def window_main():
+    fname = 'sample_data/cleaner.jls'
+
+    reader = DataReader()
+    reader.open(fname)
+
+    p = PostProcess()
+    t1 = time.time()
+    max, start, end = p.max_window(reader, duration=1)
+    print(time.time() - t1)
+    print(max, start, end)
+
+
+if __name__ == '__main__':
+    window_main()
